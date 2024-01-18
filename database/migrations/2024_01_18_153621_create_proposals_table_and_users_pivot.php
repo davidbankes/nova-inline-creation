ba81dba19acfc6eaa84eccba96dee7ba9d392f05
David Bankes <davidbankes@gmail.com>
2024-01-18 10:57:50 -0500

    fix: migration lacked timestamps

--- database/migrations/2024_01_18_153621_create_proposals_table_and_users_pivot.php
+++ database/migrations/2024_01_18_153621_create_proposals_table_and_users_pivot.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('proposals', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
+            $table->timestamps();
         });
 
         Schema::create('proposal_user', function (Blueprint $table) {
